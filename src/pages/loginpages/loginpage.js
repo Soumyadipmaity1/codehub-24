f8187c247635af890f726056f1653eb39839c6d3
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-01-31 21:06:54 +0530

    login all set

--- src/pages/loginpages/loginpage.js
+++ src/pages/loginpages/loginpage.js
@@ -1,8 +1,15 @@
- 
+ import { useState } from "react";
+ import { useForm } from "react-hook-form";
 import logo from './companylogo.svg'
  
  function Login(){
-
+const { register, handleSubmit, errors } = useForm();
+const [userInfo, setUserInfo] = useState();
+const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log(data);
+};
+console.log(errors);
     return(
         <div className="flex h-screen ">
         <div class="flex items-center w-2/3 bg-cover background" >
@@ -21,21 +28,24 @@ import logo from './companylogo.svg'
                   <h4 class="text-2xl text-gray-300 font-semibold">Login</h4>
               </div>
               <div class="w-24 ml-10 h-2 border-b-4 mb-10 border-purple-400"></div>
-  
-              <form class="w-full px-10">
+  <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+              <form onSubmit={handleSubmit(onSubmit)} class="w-full px-10">
                   <div class="pb-5">
                       <div class="mb-4 flex bg-white rounded-lg">
                           <i class='bx bx-user text-2xl py-2 px-2 text-gray-800'></i>
                       
-                          <input type="text" id="username" name="username" placeholder="Enter ID" required
+                          <input type="text" id="username" name="username" placeholder="Enter ID" ref={register({required:"Username is required"})}
                               class="w-full rounded-lg p-2  " />
                           </div>
+                          <p>{errors.username?.message}</p>
                               <div class="mb-4 bg-white flex rounded-lg">
                                   <i class='bx bx-lock-alt text-2xl text-gray-800 py-2 px-2 ' ></i>
-                          <input type="password" id="password" name="password" placeholder="Password" required
+                          <input type="password" id="password" name="password" placeholder="Password" ref={register({required:"Password is required"})}
                               class="w-full rounded-lg  p-2 " />
           
                           </div>
+                          <p>{errors.password?.message}</p>
+
                   </div>
                   <div class="items-center text-center pt-4">
                       <button type="submit"
