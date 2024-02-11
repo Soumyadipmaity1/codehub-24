b4a118d087f76f8698c67743c2fe50828a932013
Avani <avani.gopalnagar@gmail.com>
2024-02-11 22:46:39 +0530

    changes made in HomePage

---
+++ src/pages/landing/components/HomePage.js
@@ -0,0 +1,14 @@
+// HomePage.js
+
+import Footer from "./footer";
+import Offer from "./offer";
+import Loginpart from "./loginpart";
+import ImageSlider from "./imageslider";
+
+function HomePage() {
+  
+}
+
+export { Footer, Offer, Loginpart, ImageSlider }; 
+
+export default HomePage; 
