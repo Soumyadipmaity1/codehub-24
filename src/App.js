a639450a81614f6cefdfa82609a53d830156fd72
AmanGupta445052 <104879842+AmanGupta445052@users.noreply.github.com>
2024-04-02 23:00:20 +0530

    updated changes of the codeeditor

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
           />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<Error />} />
-          <Route path="/" element={<CodeEditor/>} />
+          <Route path="/codeeditor" element={<CodeEditor/>} />
     </Routes>
   );
 }
