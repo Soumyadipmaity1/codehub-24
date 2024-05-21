9f12ff1aba4b6bf687ebdf488f0568a0f3448df2
Harsh <harshku2222@gmail.com>
2024-05-22 00:20:27 +0530

    feat:change cors

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "https://codehub-24.vercel.app/", //frontend port number
+		origin: "*", //frontend port number
 		credentials: true,
 	})
 );
